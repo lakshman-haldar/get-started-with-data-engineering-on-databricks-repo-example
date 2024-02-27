3adf99bff830bf9ed986d669b03625fe261adb98
2022DC04201@WILP.BITS-PILANI.AC.IN <2022DC04201@WILP.BITS-PILANI.AC.IN>
2024-02-27 17:27:17 +0000

    add Leena

---
+++ LEENA Webinar.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+import random
+
+# Generate a random integer between 1 and 10
+random_integer = random.randint(1, 10)
+
+# Generate a random float between 0 and 1
+random_float = random.random()
+
+# Generate a random number from a normal distribution with mean 0 and standard deviation 1
+random_normal = random.normalvariate(0, 1)
